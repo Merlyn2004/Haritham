fa28144b682e4f57d3141195db4faf55add0b415
Merlyn2004 <merlynstephen2004@gmail.com>
2024-11-08 10:00:15 +0530

    Delete work.js

--- work.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('worker-login').addEventListener('click', function() {
-    document.getElementById('login-form').classList.add('active');
-    document.querySelector('form').action = '/worker-login';
-});
-
-document.getElementById('user-login').addEventListener('click', function() {
-    document.getElementById('login-form').classList.add('active');
-    document.querySelector('form').action = '/user-login';
-});
